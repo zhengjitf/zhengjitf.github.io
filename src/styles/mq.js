7fb684a249d703f820d062c3ce573321c78e1d66
zhengjitf <zhengjitf@gmail.com>
2020-01-09 21:54:23 +0800

    Updates

--- src/styles/mq.js
+++
@@ -1,7 +0,0 @@
-import facepaint from 'facepaint'
-
-const breakpoints = [768, 992, 1200]
-
-export const mq = facepaint(
-  breakpoints.map(bp => `@media (min-width: ${bp}px)`)
-)
---
+++ styles-b66b36d81aa0b9bc8a79.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{"+pfR":function(n,o,p){},GevN:function(n,o,p){},"tb+u":function(n,o,p){}}]);
+//# sourceMappingURL=styles-b66b36d81aa0b9bc8a79.js.map
